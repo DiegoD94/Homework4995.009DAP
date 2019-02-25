8e08e8598bbed7afe9ce8972ca517db09f905e5b
DH <asdfdh51@126.com>
2019-02-25 18:40:25 -0500

    done

---
+++ Homework1/app_3.py
@@ -0,0 +1,50 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.graph_objs as go
+import numpy as np
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+x = []
+y = []
+for i in np.arange(1, 20, 1):
+    x.append(i)
+    y.append(i * i)
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
+
+    html.Div(children='''
+        Dash: A web application framework for Python.
+    '''),
+
+    dcc.Graph(
+        id='x^2 scatter',
+        figure={
+            'data': [
+                go.Scatter(
+                    x=x,
+                    y=y,
+                    mode='markers',
+                    opacity=0.7,
+                    marker={
+                        'size': 15,
+                        'line': {'width': 0.5, 'color': 'white'}
+                    },
+                )
+            ],
+            'layout': go.Layout(
+                xaxis={'title': 'x'},
+                yaxis={'title': 'x square'},
+                margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
+                legend={'x': 0, 'y': 1},
+                hovermode='closest'
+            )
+        }
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
