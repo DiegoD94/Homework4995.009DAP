69d7a78e133f2e4a0c641e6eacb74f6cba06c88e
DH <asdfdh51@126.com>
2019-04-24 16:59:04 -0400

    a

---
+++ final/pytorch_keras_copy/script.py
@@ -0,0 +1,11 @@
+import os
+import sys
+image_path = sys.argv[1]
+mask_path = sys.argv[2]
+if os.listdir('./data/val_large') != []:
+    os.system('rm  ./data/val_large/*')
+if os.listdir('./mask') != []:
+    os.system('rm ./mask/*')
+os.system('cp '+str(image_path)+' ./data/val_large/')
+os.system('cp '+str(mask_path)+' ./mask/')
+os.system('python test.py --snapshot 1000000.pth')
